4faf7265414d4dafea38ab93f3bee63819a16fe2
Olga Korskova <o-korskova@yandex.ru>
2022-10-01 17:35:35 +0300

    Added PostmanEchoTest

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+
+}
